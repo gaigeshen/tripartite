1c02dec3d9e02f88b2aebda6349520e688847b73
gaigeshen <gaigeshen@qq.com>
2022-04-05 22:06:11 +0800

    优化微信支付异步通知数据体接收器

--- tripartite-pay/tripartite-pay-wechat/src/main/java/work/gaigeshen/tripartite/pay/wechat/notify/WechatNotifyBodyReceiver.java
+++ tripartite-pay/tripartite-pay-wechat/src/main/java/work/gaigeshen/tripartite/pay/wechat/notify/WechatNotifyBodyReceiver.java
@@ -1,15 +1,15 @@
 package work.gaigeshen.tripartite.pay.wechat.notify;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import work.gaigeshen.tripartite.core.notify.AbstractNotifyContentReceiver;
 import work.gaigeshen.tripartite.core.notify.NotifyContentIncorrectException;
-import work.gaigeshen.tripartite.core.util.JacksonUtils;
+import work.gaigeshen.tripartite.core.util.json.JsonCodec;
 import work.gaigeshen.tripartite.pay.wechat.WechatClients;
 import work.gaigeshen.tripartite.pay.wechat.config.WechatCertificates;
 import work.gaigeshen.tripartite.pay.wechat.config.WechatConfig;
 import work.gaigeshen.tripartite.pay.wechat.config.WechatSecretKey;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -31,13 +31,13 @@ public class WechatNotifyBodyReceiver extends AbstractNotifyContentReceiver<Wech
   @Override
   protected WechatNotifyBody validate(WechatNotifyBody body) throws NotifyContentIncorrectException {
     String bodyString = body.getBodyAsString();
-    JsonNode jsonNode = JacksonUtils.toJsonNode(bodyString);
+    Map<String, Object> bodyDecoded = JsonCodec.instance().decodeObject(bodyString);
 
-    JsonNode eventTypeJsonNode = jsonNode.get("event_type");
-    if (Objects.isNull(eventTypeJsonNode) || !eventTypeJsonNode.isTextual()) {
+    String eventType = (String) bodyDecoded.get("event_type");
+    if (Objects.isNull(eventType)) {
       throw new NotifyContentIncorrectException("notify event type not found: " + body);
     }
-    body.setEventType(eventTypeJsonNode.textValue());
+    body.setEventType(eventType);
 
     String timestamp = body.getHeaderValue("Wechatpay-Timestamp");
     if (Objects.isNull(timestamp)) {
@@ -64,29 +64,29 @@ public class WechatNotifyBodyReceiver extends AbstractNotifyContentReceiver<Wech
       throw new NotifyContentIncorrectException("sign is invalid: " + body);
     }
 
-    JsonNode resourceJsonNode = jsonNode.get("resource");
-    if (Objects.isNull(resourceJsonNode)) {
+    Map<?, ?> resource = (Map<?, ?>) bodyDecoded.get("resource");
+    if (Objects.isNull(resource)) {
       throw new NotifyContentIncorrectException("notify resource not found: " + body);
     }
-    JsonNode ciphertextJsonNode = resourceJsonNode.get("ciphertext");
-    if (Objects.isNull(ciphertextJsonNode) || !ciphertextJsonNode.isTextual()) {
+    String resourceCiphertext = (String) resource.get("ciphertext");
+    if (Objects.isNull(resourceCiphertext)) {
       throw new NotifyContentIncorrectException("notify ciphertext resource not found: " + body);
     }
-    JsonNode nonceJsonNode = resourceJsonNode.get("nonce");
-    if (Objects.isNull(nonceJsonNode) || !ciphertextJsonNode.isTextual()) {
+    String resourceNonce = (String) resource.get("nonce");
+    if (Objects.isNull(resourceNonce)) {
       throw new NotifyContentIncorrectException("notify nonce resource not found: " + body);
     }
-    JsonNode associatedDataJsonNode = resourceJsonNode.get("associated_data");
+    String resourceAssociatedData = (String) resource.get("associated_data");
 
-    byte[] nonceBytes = nonceJsonNode.textValue().getBytes(StandardCharsets.UTF_8);
+    byte[] nonceBytes = resourceNonce.getBytes(StandardCharsets.UTF_8);
     byte[] associatedDataBytes;
-    if (Objects.isNull(associatedDataJsonNode) || !associatedDataJsonNode.isTextual()) {
+    if (Objects.isNull(resourceAssociatedData)) {
       associatedDataBytes = new byte[0];
     } else {
-      associatedDataBytes = associatedDataJsonNode.textValue().getBytes(StandardCharsets.UTF_8);
+      associatedDataBytes = resourceAssociatedData.getBytes(StandardCharsets.UTF_8);
     }
     WechatSecretKey secretKey = config.getSecretKey();
-    byte[] decrypted = secretKey.decrypt(ciphertextJsonNode.textValue(), nonceBytes, associatedDataBytes);
+    byte[] decrypted = secretKey.decrypt(resourceCiphertext, nonceBytes, associatedDataBytes);
 
     body.setResource(new String(decrypted, StandardCharsets.UTF_8));
 
