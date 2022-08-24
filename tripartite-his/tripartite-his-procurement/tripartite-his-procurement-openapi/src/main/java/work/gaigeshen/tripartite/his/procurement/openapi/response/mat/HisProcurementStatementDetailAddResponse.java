5dd4e455db601c10ffa40f562791bb74cd59680e
gaigeshen <gaigeshen@qq.com>
2022-08-24 10:20:40 +0800

    添加结算单明细返回的结果修正

--- tripartite-his/tripartite-his-procurement/tripartite-his-procurement-openapi/src/main/java/work/gaigeshen/tripartite/his/procurement/openapi/response/mat/HisProcurementStatementDetailAddResponse.java
+++ tripartite-his/tripartite-his-procurement/tripartite-his-procurement-openapi/src/main/java/work/gaigeshen/tripartite/his/procurement/openapi/response/mat/HisProcurementStatementDetailAddResponse.java
@@ -9,13 +9,46 @@ import java.util.Collection;
  */
 public class HisProcurementStatementDetailAddResponse extends AbstractHisProcurementResponse {
 
-  private Collection<String> dataList;
+  private Collection<ListItem> dataList;
 
-  public Collection<String> getDataList() {
+  public Collection<ListItem> getDataList() {
     return dataList;
   }
 
-  public void setDataList(Collection<String> dataList) {
+  public void setDataList(Collection<ListItem> dataList) {
     this.dataList = dataList;
+  }
+
+  public static class ListItem {
+
+    private Integer returnCode;
+
+    private String returnMsg;
+
+    private String payyDetlId;
+
+    public Integer getReturnCode() {
+      return returnCode;
+    }
+
+    public void setReturnCode(Integer returnCode) {
+      this.returnCode = returnCode;
+    }
+
+    public String getReturnMsg() {
+      return returnMsg;
+    }
+
+    public void setReturnMsg(String returnMsg) {
+      this.returnMsg = returnMsg;
+    }
+
+    public String getPayyDetlId() {
+      return payyDetlId;
+    }
+
+    public void setPayyDetlId(String payyDetlId) {
+      this.payyDetlId = payyDetlId;
+    }
   }
 }
