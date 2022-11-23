1d472732d93bb6a0863df07922cc4b31f0dafe5c
gaigeshen <gaigeshen@qq.com>
2022-11-23 18:59:14 +0800

    钉钉增加样例模块

---
+++ tripartite-ding/tripartite-ding-sample/src/main/java/work/gaigeshen/tripartite/ding/sample/DingSampleApplication.java
@@ -0,0 +1,16 @@
+package work.gaigeshen.tripartite.ding.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author gaigeshen
+ */
+@SpringBootApplication
+public class DingSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingSampleApplication.class, args);
+    }
+}
