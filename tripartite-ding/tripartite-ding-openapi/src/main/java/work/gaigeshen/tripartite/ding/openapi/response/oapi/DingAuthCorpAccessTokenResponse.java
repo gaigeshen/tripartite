d3a1d9a7e3acc6d85b9fcdd97e86397e98d68c33
gaigeshen <gaigeshen@qq.com>
2022-11-25 09:07:08 +0800

    授权企业访问令牌参数和响应结果

---
+++ tripartite-ding/tripartite-ding-openapi/src/main/java/work/gaigeshen/tripartite/ding/openapi/response/oapi/DingAuthCorpAccessTokenResponse.java
@@ -0,0 +1,14 @@
+package work.gaigeshen.tripartite.ding.openapi.response.oapi;
+
+import work.gaigeshen.tripartite.ding.openapi.response.DingOapiResponse;
+
+/**
+ *
+ * @author gaigeshen
+ */
+public class DingAuthCorpAccessTokenResponse extends DingOapiResponse {
+
+    public String accessToken;
+
+    public Long expireIn;
+}
