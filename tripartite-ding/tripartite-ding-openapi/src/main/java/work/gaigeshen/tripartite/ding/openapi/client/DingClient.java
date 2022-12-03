7bd924a73f10cc11a93dc4de8c37405d9bb6412b
gaigeshen <gaigeshen@qq.com>
2022-12-04 00:37:01 +0800

    去除不需要的代码

--- tripartite-ding/tripartite-ding-openapi/src/main/java/work/gaigeshen/tripartite/ding/openapi/client/DingClient.java
+++ tripartite-ding/tripartite-ding-openapi/src/main/java/work/gaigeshen/tripartite/ding/openapi/client/DingClient.java
@@ -1,10 +1,5 @@
 package work.gaigeshen.tripartite.ding.openapi.client;
 
-import work.gaigeshen.tripartite.core.client.ClientException;
-import work.gaigeshen.tripartite.ding.openapi.config.DingAuthCorpContexts;
-import work.gaigeshen.tripartite.ding.openapi.config.DingAuthCorpContexts.Context;
-import work.gaigeshen.tripartite.ding.openapi.config.DingConfig;
-
 import java.util.Objects;
 
 /**
@@ -14,39 +9,21 @@ import java.util.Objects;
  */
 public interface DingClient extends DingApiClient, DingOapiClient {
 
-    default String getAuthCorpId() {
-        Context authCorpContext = getAuthCorpContext();
-        if (Objects.isNull(authCorpContext)) {
-            return null;
-        }
-        return authCorpContext.getAuthCorpId();
-    }
-
-    default boolean hasAuthCorpContext() {
-        return Objects.nonNull(getAuthCorpContext());
-    }
-
-    default Context getAuthCorpContext() {
-        return DingAuthCorpContexts.getContextLocal();
-    }
-
-    default String getSuiteTicket() throws ClientException {
-        DingConfig config = getConfig();
-        if (Objects.isNull(config.getSuiteId())) {
-            throw new ClientException("suite id not found: " + config);
-        }
-        DingSuiteTicketStore suiteTicketStore = getSuiteTicketStore();
-        if (Objects.isNull(suiteTicketStore)) {
-            throw new ClientException("suite ticket store not found: " + config);
-        }
-        return suiteTicketStore.getTicket(config.getSuiteId());
-    }
-
+    /**
+     * 返回是否有套件票据存储器，只有第三方企业应用开发才需要
+     *
+     * @return 是否有套件票据存储器
+     */
     default boolean hasSuiteTicketStore() {
         return Objects.nonNull(getSuiteTicketStore());
     }
 
-    default DingSuiteTicketStore getSuiteTicketStore() throws ClientException {
+    /**
+     * 返回钉钉套件票据存储器
+     *
+     * @return 钉钉套件票据存储器，默认返回空对象，表示没有套件票据存储器
+     */
+    default DingSuiteTicketStore getSuiteTicketStore() {
         return null;
     }
 }
--- tripartite-ding/tripartite-ding-openapi/src/main/java/work/gaigeshen/tripartite/ding/openapi/config/DingAuthCorpContexts.java
+++ tripartite-ding/tripartite-ding-openapi/src/main/java/work/gaigeshen/tripartite/ding/openapi/config/DingAuthCorpContexts.java
@@ -3,6 +3,7 @@ package work.gaigeshen.tripartite.ding.openapi.config;
 import java.util.Objects;
 
 /**
+ * 钉钉授权企业上下文信息保存，采用当前线程变量的形式
  *
  * @author gaigeshen
  */
@@ -10,18 +11,36 @@ public class DingAuthCorpContexts {
 
     private static final ThreadLocal<Context> CONTEXT_LOCAL = new ThreadLocal<>();
 
+    /**
+     * 设置当前的授权企业上下文信息
+     *
+     * @param context 当前的授权企业上下文信息
+     */
     public static void setContextLocal(Context context) {
         CONTEXT_LOCAL.set(context);
     }
 
+    /**
+     * 获取当前的授权企业上下文信息
+     *
+     * @return 当前的授权企业上下文信息
+     */
     public static Context getContextLocal() {
         return CONTEXT_LOCAL.get();
     }
 
+    /**
+     * 移除当前的授权企业上下文信息，在不需要的时候进行移除操作
+     */
     public static void removeContextLocal() {
         CONTEXT_LOCAL.remove();
     }
 
+    /**
+     * 钉钉授权企业上下文信息
+     *
+     * @author gaigeshen
+     */
     public static class Context extends DingConfig {
 
         private final String authCorpId;
