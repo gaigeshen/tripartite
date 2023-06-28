5f5e448d58436b98d476b312414e2d42c190afe3
gaigeshen <gaigeshen@qq.com>
2023-06-28 18:03:19 +0800

    钉钉代办任务创建和更新

---
+++ tripartite-ding/tripartite-ding-openapi/src/main/java/work/gaigeshen/tripartite/ding/openapi/parameters/api/task/DingTaskCreateParameters.java
@@ -0,0 +1,28 @@
+package work.gaigeshen.tripartite.ding.openapi.parameters.api.task;
+
+import work.gaigeshen.tripartite.core.parameter.converter.JsonParametersConverter;
+import work.gaigeshen.tripartite.core.parameter.converter.Parameters;
+import work.gaigeshen.tripartite.ding.openapi.parameters.DingApiParameters;
+
+import java.util.Collection;
+
+/**
+ * @author gaigeshen
+ */
+@Parameters(
+        converter = JsonParametersConverter.class
+)
+public class DingTaskCreateParameters extends DingApiParameters {
+
+    public String subject;
+
+    public String description;
+
+    public Long dueTime;
+
+    public Long priority;
+
+    public Collection<String> executorIds;
+
+    public Collection<String> participantIds;
+}
