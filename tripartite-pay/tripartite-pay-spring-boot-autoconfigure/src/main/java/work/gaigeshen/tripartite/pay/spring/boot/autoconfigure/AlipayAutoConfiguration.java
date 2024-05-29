21c6d986cecfa8b69090e838e57fd0861efa2fe6
gaigeshen <gaigeshen@qq.com>
2024-05-29 18:07:44 +0800

    微信支付工具类

--- tripartite-pay/tripartite-pay-spring-boot-autoconfigure/src/main/java/work/gaigeshen/tripartite/pay/spring/boot/autoconfigure/AlipayAutoConfiguration.java
+++ tripartite-pay/tripartite-pay-spring-boot-autoconfigure/src/main/java/work/gaigeshen/tripartite/pay/spring/boot/autoconfigure/AlipayAutoConfiguration.java
@@ -21,86 +21,85 @@ import java.util.Collections;
 import java.util.List;
 
 /**
- *
  * @author gaigeshen
  */
-@EnableConfigurationProperties({ AlipayProperties.class })
-@ConditionalOnClass({ AlipayClient.class })
+@EnableConfigurationProperties({AlipayProperties.class})
+@ConditionalOnClass({AlipayClient.class})
 @Configuration
 public class AlipayAutoConfiguration {
 
-  private static final Logger log = LoggerFactory.getLogger(AlipayAutoConfiguration.class);
+    private static final Logger log = LoggerFactory.getLogger(AlipayAutoConfiguration.class);
 
-  private final AlipayProperties alipayProperties;
+    private final AlipayProperties alipayProperties;
 
-  private final List<AbstractNotifyContentProcessor<AlipayNotifyParameters>> processors;
+    private final List<AbstractNotifyContentProcessor<AlipayNotifyParameters>> processors;
 
-  public AlipayAutoConfiguration(AlipayProperties alipayProperties, List<AbstractNotifyContentProcessor<AlipayNotifyParameters>> processors) {
-    this.alipayProperties = alipayProperties;
-    this.processors = processors;
-  }
-
-  @Bean
-  public FilterRegistrationBean alipayNotifyParametersFilter(AlipayNotifyParametersReceiver receiver) {
-    AlipayNotifyParametersFilter filter = new AlipayNotifyParametersFilter(receiver);
-    FilterRegistrationBean filterBean = new FilterRegistrationBean();
-    filterBean.setUrlPatterns(Collections.singletonList("/alipay-notify-receiver"));
-    filterBean.setFilter(filter);
-    return filterBean;
-  }
-
-  @Bean
-  public AlipayNotifyParametersReceiver alipayNotifyParametersReceiver(AlipayClients alipayClients) {
-    AlipayNotifyParametersReceiver receiver = new AlipayNotifyParametersReceiver(alipayClients);
-    receiver.setProcessors(new ArrayList<>(processors));
-    return receiver;
-  }
-
-  @Bean
-  public AlipayClients alipayClients() {
-
-    Collection<AlipayClient> alipayClients = new ArrayList<>();
-
-    for (AlipayProperties.Client client : alipayProperties.getClients()) {
-      AlipayConfig.Builder builder = AlipayConfig.builder();
-
-      if (!StringUtils.hasText(client.getAppId()) || !StringUtils.hasText(client.getServerUrl())) {
-        throw new IllegalStateException("appId and server url cannot be null or empty");
-      }
-      builder.setAppId(client.getAppId());
-      builder.setServerUrl(client.getServerUrl());
-
-      if (!StringUtils.hasText(client.getNotifyServerHost())) {
-        throw new IllegalStateException("notify server host cannot be null or empty");
-      }
-      builder.setNotifyUrl(client.getNotifyServerHost() + "/alipay-notify-receiver");
-
-      AlipayProperties.Client.Certificates certificates = client.getCertificates();
-      switch (certificates.getType()) {
-        case CLASSPATH:
-          builder.setPrivateKeyClasspath(certificates.getAppPrivateKey(), certificates.getAppCertificate());
-          builder.setRootCertificateClasspath(certificates.getRootCertificate());
-          builder.setCertificateClasspath(certificates.getCertificate());
-          break;
-        case FILE:
-          builder.setPrivateKeyFile(certificates.getAppPrivateKey(), certificates.getAppCertificate());
-          builder.setRootCertificateFile(certificates.getRootCertificate());
-          builder.setCertificateFile(certificates.getCertificate());
-          break;
-        case CONTENT:
-          builder.setPrivateKeyContent(certificates.getAppPrivateKey(), certificates.getAppCertificate());
-          builder.setRootCertificateContent(certificates.getRootCertificate());
-          builder.setCertificateContent(certificates.getCertificate());
-          break;
-      }
-      AlipayConfig alipayConfig = builder.build();
-
-      alipayClients.add(DefaultAlipayClient.create(alipayConfig, new AlipayResponseInterceptor(alipayConfig)));
-
-      log.info("loaded alipay client: {}", alipayConfig);
+    public AlipayAutoConfiguration(AlipayProperties alipayProperties, List<AbstractNotifyContentProcessor<AlipayNotifyParameters>> processors) {
+        this.alipayProperties = alipayProperties;
+        this.processors = processors;
     }
-    return new DefaultAlipayClients(alipayClients);
-  }
+
+    @Bean
+    public FilterRegistrationBean alipayNotifyParametersFilter(AlipayNotifyParametersReceiver receiver) {
+        AlipayNotifyParametersFilter filter = new AlipayNotifyParametersFilter(receiver);
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setUrlPatterns(Collections.singletonList("/alipay-notify-receiver"));
+        filterBean.setFilter(filter);
+        return filterBean;
+    }
+
+    @Bean
+    public AlipayNotifyParametersReceiver alipayNotifyParametersReceiver(AlipayClients alipayClients) {
+        AlipayNotifyParametersReceiver receiver = new AlipayNotifyParametersReceiver(alipayClients);
+        receiver.setProcessors(new ArrayList<>(processors));
+        return receiver;
+    }
+
+    @Bean
+    public AlipayClients alipayClients() {
+
+        Collection<AlipayClient> alipayClients = new ArrayList<>();
+
+        for (AlipayProperties.Client client : alipayProperties.getClients()) {
+            AlipayConfig.Builder builder = AlipayConfig.builder();
+
+            if (!StringUtils.hasText(client.getAppId()) || !StringUtils.hasText(client.getServerUrl())) {
+                throw new IllegalStateException("appId and server url cannot be null or empty");
+            }
+            builder.setAppId(client.getAppId());
+            builder.setServerUrl(client.getServerUrl());
+
+            if (!StringUtils.hasText(client.getNotifyServerHost())) {
+                throw new IllegalStateException("notify server host cannot be null or empty");
+            }
+            builder.setNotifyUrl(client.getNotifyServerHost() + "/alipay-notify-receiver");
+
+            AlipayProperties.Client.Certificates certificates = client.getCertificates();
+            switch (certificates.getType()) {
+                case CLASSPATH:
+                    builder.setPrivateKeyClasspath(certificates.getAppPrivateKey(), certificates.getAppCertificate());
+                    builder.setRootCertificateClasspath(certificates.getRootCertificate());
+                    builder.setCertificateClasspath(certificates.getCertificate());
+                    break;
+                case FILE:
+                    builder.setPrivateKeyFile(certificates.getAppPrivateKey(), certificates.getAppCertificate());
+                    builder.setRootCertificateFile(certificates.getRootCertificate());
+                    builder.setCertificateFile(certificates.getCertificate());
+                    break;
+                case CONTENT:
+                    builder.setPrivateKeyContent(certificates.getAppPrivateKey(), certificates.getAppCertificate());
+                    builder.setRootCertificateContent(certificates.getRootCertificate());
+                    builder.setCertificateContent(certificates.getCertificate());
+                    break;
+            }
+            AlipayConfig alipayConfig = builder.build();
+
+            alipayClients.add(DefaultAlipayClient.create(alipayConfig, new AlipayResponseInterceptor(alipayConfig)));
+
+            log.info("loaded alipay client: {}", alipayConfig);
+        }
+        return new DefaultAlipayClients(alipayClients);
+    }
 
 
 }
