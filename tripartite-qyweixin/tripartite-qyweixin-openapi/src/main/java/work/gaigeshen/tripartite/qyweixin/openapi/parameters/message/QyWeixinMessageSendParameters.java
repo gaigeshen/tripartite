3762bf2f918efc1bd1c13295ccf90ae827ebf057
gaigeshen <gaigeshen@qq.com>
2024-10-03 19:48:42 +0800

    企业微信发送应用消息增加文本卡片类型

--- tripartite-qyweixin/tripartite-qyweixin-openapi/src/main/java/work/gaigeshen/tripartite/qyweixin/openapi/parameters/message/QyWeixinMessageSendParameters.java
+++ tripartite-qyweixin/tripartite-qyweixin-openapi/src/main/java/work/gaigeshen/tripartite/qyweixin/openapi/parameters/message/QyWeixinMessageSendParameters.java
@@ -4,8 +4,6 @@ import work.gaigeshen.tripartite.core.parameter.converter.JsonParametersConverte
 import work.gaigeshen.tripartite.core.parameter.converter.Parameters;
 import work.gaigeshen.tripartite.qyweixin.openapi.parameters.QyWeixinParameters;
 
-import java.util.List;
-
 /**
  *
  * @author gaigeshen
@@ -27,38 +25,18 @@ public class QyWeixinMessageSendParameters extends QyWeixinParameters {
 
     public Markdown markdown;
 
-    public MiniprogramNotice miniprogram_notice;
+    public static class TextCard {
+
+        public String title;
+
+        public String description;
+
+        public String url;
+    }
 
     public static class Markdown {
 
         public String content;
     }
 
-    public static class MiniprogramNotice {
-
-        public String appid;
-
-        public String page;
-
-        public String title;
-
-        public String description;
-
-        public Boolean emphasis_first_item;
-
-        public List<MiniprogramNoticeContentItem> content_item;
-
-        public Integer enable_id_trans;
-
-        public Integer enable_duplicate_check;
-
-        public Integer duplicate_check_interval;
-    }
-
-    public static class MiniprogramNoticeContentItem {
-
-        public String key;
-
-        public String value;
-    }
 }
